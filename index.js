f9d7946beafb5675d20ada38bc320b7ef9d6a184
iamrivera <a3m3rivera@gmail.com>
2021-07-15 20:04:09 -0700

    commit tree 0eb7202510a5ea6953318c3dd7d2b43cd603bb39

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+const multiply = num1*num2 
+
+const random = Math.floor(Math.random()*10)
+
+let num3 = 32
+let num4 = 7
+
+const mod = 32 % 7
+
+const max = Math.max(1,2,3,20)
+
